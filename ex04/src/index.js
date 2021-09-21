40b163e189a722a32e0862b6cc39aeb5198f0cab
kenoaljiji <studioaljiji@gmail.com>
2021-09-22 00:18:47 +0200

    Compare two numbers and return boolean value

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+function compareDifferentValues(m, n) {
+  //Only change code below this line
+  if (m === n) {
+    return "Equal";
+  } else {
+    return "Not equal";
+  }
+  //Only change code above this line
+}
+console.log(compareDifferentValues(8, "8"));
+console.log(compareDifferentValues("8", 8));
+console.log(compareDifferentValues(`8`, 8)); // I cannot find this symbol
+console.log(compareDifferentValues("8", "8"));
+console.log(compareDifferentValues(8, 8));
+
+module.exports = compareDifferentValues;
