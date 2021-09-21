ab211a652df2d3041da6d71cd86123c6c7a4e34a
kenoaljiji <studioaljiji@gmail.com>
2021-09-21 23:44:43 +0200

    Add function that return boolean value

---
+++ ex01/src/index.js
@@ -0,0 +1,8 @@
+function myTrueBoolean() {
+  //Only change code below this line
+  return true;
+  //Only change code above this line
+}
+console.log(myTrueBoolean());
+
+module.exports = myTrueBoolean;
