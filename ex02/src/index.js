ff426afb2fe411a1cca385ea5518bca871ebaed9
kenoaljiji <studioaljiji@gmail.com>
2021-09-21 23:54:02 +0200

    return a function in conditional statements

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,13 +1,13 @@
 function myFunction(myParamater) {
   //Only change code below this line
   if (myParamater) {
-    return console.log("The paramater is true!");
+    return "The paramater is true!";
   } else {
-    return console.log("The paramater is false!");
+    return "The paramater is false!";
   }
   //Only change code above this line
 }
-myFunction(true);
-myFunction(false);
+console.log(myFunction(true));
+console.log(myFunction(false));
 
 module.exports = myFunction;
