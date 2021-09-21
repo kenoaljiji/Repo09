e56b3cbcc38a70342d60262f67faa922417d79a0
kenoaljiji <studioaljiji@gmail.com>
2021-09-22 00:50:37 +0200

    Compare boolian value with paramater with  use of operators

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,16 +1,25 @@
-function compareStrictValues(m) {
+function comparisonToEqual(a) {
   //Only change code below this line
-  if (m != 55) {
-    return "Not equal";
-  } else {
-    return "Equal";
+  if (a < 5) {
+    return "Less than 5";
   }
+  if (a < 10) {
+    return "Less than 10";
+  }
+
+  if (a > 20) {
+    return "More than 20";
+  }
+
+  if (a >= 10) {
+    return "10 or over";
+  }
+
   //Only change code above this line
 }
-console.log(compareStrictValues(55));
-console.log(compareStrictValues("55"));
-console.log(compareStrictValues("21"));
-console.log(compareStrictValues("12"));
-console.log(compareStrictValues("Doe"));
+console.log(comparisonToEqual(0));
+console.log(comparisonToEqual(5));
+console.log(comparisonToEqual(17));
+console.log(comparisonToEqual(21));
 
-module.exports = compareStrictValues;
+module.exports = comparisonToEqual;
