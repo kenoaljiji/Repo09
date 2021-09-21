3ad860308648d76de289b7e22d1c44994a170b3b
kenoaljiji <studioaljiji@gmail.com>
2021-09-22 00:01:02 +0200

    Compare two numbert with function that return Boolean statement

---
+++ ex03/src/index.js
@@ -0,0 +1,16 @@
+function myEqualFunction(num) {
+  //Only change code below this line
+  if (num === 23) {
+    return "Equal";
+  } else {
+    return "Not equal";
+  }
+  //Only change code above this line
+}
+console.log(myEqualFunction(5));
+console.log(myEqualFunction(23));
+console.log(myEqualFunction("23"));
+console.log(myEqualFunction("23"));
+console.log(myEqualFunction("text"));
+
+module.exports = myEqualFunction;
