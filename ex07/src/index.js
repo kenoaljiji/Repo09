5866a79d607a0d88281b10cad7a471a3446337f7
kenoaljiji <studioaljiji@gmail.com>
2021-09-22 01:03:44 +0200

    Comparison with the logical and operator

---
+++ ex07/src/index.js
@@ -0,0 +1,17 @@
+function logicalAndOperator(num) {
+  //Only change code below this
+  if (num <= 80 && num >= 40) {
+    return "Yes";
+  } else {
+    return "No";
+  }
+
+  //Only change code above this line
+}
+console.log(logicalAndOperator(0));
+console.log(logicalAndOperator(24));
+console.log(logicalAndOperator(45));
+console.log(logicalAndOperator(57));
+console.log(logicalAndOperator(100));
+
+module.exports = logicalAndOperator;
