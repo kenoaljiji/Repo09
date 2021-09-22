da0882205c23c241a7f81d977e00ae0edeee9100
kenoaljiji <studioaljiji@gmail.com>
2021-09-22 05:02:56 +0200

    do while loop

---
+++ ex15/src/index.js
@@ -0,0 +1,20 @@
+function myDoWhile() {
+  //Only change code below this
+
+  var myNumbers = "";
+
+  var i = 0;
+  do {
+    myNumbers += i;
+    if (i === 9) break;
+    myNumbers += ", ";
+    i++;
+  } while (i <= 9);
+
+  return myNumbers;
+  //Only change code above this
+}
+
+console.log(myDoWhile());
+
+module.exports = myDoWhile;
