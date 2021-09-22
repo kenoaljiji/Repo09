b1652aec52511d219876ba28603b2c85429bcad5
kenoaljiji <studioaljiji@gmail.com>
2021-09-22 02:03:14 +0200

    Logical order in If Else Statements

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function logicOrder(num) {
+  //Only change code below this
+
+  if (num < 50) {
+    return "Less than 50";
+  } else if (num < 100) {
+    return "Less than 100";
+  } else;
+  return "Greater than equal to 100";
+
+  //Only change code above this line
+}
+console.log(logicOrder(28));
+console.log(logicOrder(60));
+console.log(logicOrder(110));
+
+module.exports = logicOrder;
